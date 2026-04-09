d3941df260f4d15bab2784a26ff41b16ccb7f228
suhaibmowaffaqaziz-tech <suhaibmowaffaqaziz@gmail.com>
2026-04-09 21:12:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿// ======================= script.js =======================
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+function checkAnswer(btn, correct) {
+  if (correct) {
+    btn.classList.add('correct');
+    btn.innerText += " (إجابة صحيحة!)";
+  } else {
+    btn.classList.add('wrong');
+    btn.innerText += " (حاول مرة أخرى)";
+  }
+}
